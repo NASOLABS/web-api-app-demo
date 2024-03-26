38fecda70c19bc783b9b956fa4b96809ed6898f1
Shiva Thirumazhusai <shiva@nasotech.com>
2024-03-26 18:24:25 -0400

    Log ..

--- services/python/health_status.py
+++ services/python/health_status.py
@@ -4,4 +4,5 @@ health_status_blueprint = Blueprint('health_status_blueprint', __name__, url_pre
 
 @health_status_blueprint.route('/health')
 def get_health_status():
+    print("Calling /health Endpoint")
     return "Python Server HEALTH STATUS is ok"
--- services/python/server.py
+++ services/python/server.py
@@ -3,11 +3,15 @@ from flask_cors import CORS
 from health_status import health_status_blueprint
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
+
+print("Registering endpoint")
+
 app.register_blueprint(health_status_blueprint)
 
 @app.route('/',methods=['GET'])
 def top_func():
+        print("Calling / Endpoint")
         return "Python Server ok"
 
 if __name__ == '__main__':
