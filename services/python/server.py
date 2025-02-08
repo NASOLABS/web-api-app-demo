338719c2b9ea3c2f7b78669c47f5970b43a9056d
Shiva Thirumazhusai <shiva@nasotech.com>
2025-02-08 07:41:17 -0500

    Applied Flak8 and Black

--- services/python/server.py
+++ services/python/server.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_cors import CORS
+# from flask_cors import CORS
 from health_status import health_status_blueprint
 
 app = Flask(__name__)
@@ -9,10 +9,12 @@ print("Registering endpoint")
 
 app.register_blueprint(health_status_blueprint)
 
-@app.route('/',methods=['GET'])
-def top_func():
-        print("Calling / Endpoint")
-        return "Python Server ok"
 
-if __name__ == '__main__':
+@app.route("/", methods=["GET"])
+def top_func():
+    print("Calling / Endpoint")
+    return "Python Server ok - Hello Intens"
+
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
